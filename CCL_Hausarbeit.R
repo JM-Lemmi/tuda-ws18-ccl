4fa9ecd7adbe6c3b7ea92fdd5179db3222c98815
JM-Lemmi <julian.lemmerich@stud.tu-darmstadt.de>
2020-10-21 18:06:40 +0200

    v2

--- CCL_Hausarbeit.R
+++ CCL_Hausarbeit.R
@@ -1,6 +1,34 @@
-#das ist der stylo part
+# Julian Lemmerich
+
 install.packages("stylo")
+install.packages("rvest")
+install.packages("tm")
+install.packages("xml2")
+
+# Scraper
+
+library(rvest)
+library(tm)
+library(xml2)
+
+# Diese Funktion scrapt alle Links
+## FUKNTIONIERT NICHT!
+url <- "https://www.americanrhetoric.com/barackobamaspeeches.htm"
+pg <- read_html(url)
+html_attr(html_nodes(pg, css = "a font"), "href")
+
+
+
+
+# Stylo
+
 library(stylo)
 
-setwd("C:/Users/julia/OneDrive/User Data/Uni/semester 3/CCL/Hausarbeit/Corpus/")
+setwd("C:/Users/julian.lemmerich/OneDrive/User Data/Uni/Semester old/Semester 3/CCL/Hausarbeit")
 stylo()
+
+
+
+### CORPUS CLEANUP
+
+
